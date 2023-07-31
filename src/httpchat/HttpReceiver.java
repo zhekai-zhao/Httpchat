c266316b3738c8aeb34c2caaef679798ebaed0c0
zhekai-zhao <zhekai1@ualberta.ca>
2023-07-31 17:46:33 -0600

    POST become magic num

--- src/httpchat/HttpReceiver.java
+++ src/httpchat/HttpReceiver.java
@@ -1,6 +1,5 @@
 package httpchat;
 
-
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -8,78 +7,92 @@ import java.net.Socket;
 public class HttpReceiver {
     private ServerSocket serverSocket;
     private Socket socket;
-    private PrintWriter writer;
-    private BufferedReader reader;
+    private DataInputStream inputStream;
     private static final int BUFFER_SIZE = 4096;
 
     public HttpReceiver(int port) throws IOException {
         this.serverSocket = new ServerSocket(port);
         this.socket = serverSocket.accept();
-        this.writer = new PrintWriter(socket.getOutputStream(), true);
-        this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.inputStream = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
     }
 
-    public void listenForCommands() throws IOException {
-        String requestLine = reader.readLine();
-        int contentLength = 0;
-
-        if (requestLine != null && requestLine.startsWith("POST")) {
+    public void listenForMessages() {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
             String line;
-            while (!(line = reader.readLine()).isEmpty()) {
-                if (line.startsWith("Content-Length:")) {
-                    contentLength = Integer.parseInt(line.split(":")[1].trim());
+
+            while (true) {
+                line = reader.readLine();
+                if (line == null) break;  // End of stream
+
+                if (line.startsWith("GET")) {
+                    String path = line.split(" ")[1];
+
+                    if ("/hello".equals(path)) {
+                        System.out.println("Connection message: Hello from Sender!");
+                    } else if ("/goodbye".equals(path)) {
+                        System.out.println("Termination message: Goodbye from Sender!");
+                        return;  // Exit the loop on receiving termination command
+                    }
+
+                    // Skip rest of the headers
+                    while (!"".equals(line = reader.readLine()));
+
+                } else if (line.startsWith("POST")) {
+                    // Skip all header lines until the blank line
+                    while (!"".equals(line = reader.readLine()));
+
+                    int command = inputStream.readInt();
+
+                    switch (command) {
+                        case 1:  // This is a message
+                            String message = inputStream.readUTF();
+                            System.out.println("Received message: " + message);
+                            break;
+                        case 2:  // This is a file
+                            receiveFile();
+                            System.out.println("File received.");
+                            break;
+                        default:
+                            System.err.println("Unknown command received: " + command);
+                            break;
+                    }
                 }
             }
-
-            char[] contentChars = new char[contentLength];
-            reader.read(contentChars, 0, contentLength);
-            String content = new String(contentChars);
-
-            int command = Integer.parseInt(content.substring(0, 1));
-            content = content.substring(1); // Remove command
-
-            switch (command) {
-                case 1:
-                    System.out.println("Received message: " + content);
-                    break;
-
-                case 2:
-                    receiveFile(content);
-                    break;
-
-                default:
-                    System.err.println("Unknown command received: " + command);
-                    break;
-            }
-
-            writer.println("HTTP/1.1 200 OK");
-            writer.println("Content-Length: 0");
-            writer.println();
-            writer.flush();
+        } catch (IOException e) {
+            System.err.println("Error occurred while reading from the socket: " + e.getMessage());
         }
     }
 
-    public void receiveFile(String content) throws IOException {
-        String[] fileParts = content.split("\n", 2);  // Split only into two parts
-        String fileName = fileParts[0];
-        byte[] fileData = fileParts[1].getBytes();
 
-        try (FileOutputStream fos = new FileOutputStream(fileName)) {
-            ByteArrayInputStream bis = new ByteArrayInputStream(fileData);
-            byte[] buffer = new byte[BUFFER_SIZE];
-            int bytesRead = 0;
-            
-            while ((bytesRead = bis.read(buffer)) != -1) {
-                fos.write(buffer, 0, bytesRead);
-            }
+
+    public void receiveFile() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String fileDetails = reader.readLine();
+
+        String[] details = fileDetails.split(":");
+        String fileName = details[1];
+        long fileSize = Long.parseLong(details[2]);
+
+        File file = new File(fileName);
+        FileOutputStream fos = new FileOutputStream(file);
+
+        byte[] buffer = new byte[BUFFER_SIZE];
+        long remaining = fileSize;
+
+        while (remaining > 0) {
+            int bytesRead = inputStream.read(buffer, 0, (int)Math.min(BUFFER_SIZE, remaining));
+            fos.write(buffer, 0, bytesRead);
+            remaining -= bytesRead;
         }
 
-        System.out.println("File received: " + fileName);
+        fos.close();
     }
+
+
 
     public void close() throws IOException {
-        writer.close();
-        reader.close();
+        inputStream.close();
         socket.close();
         serverSocket.close();
     }
@@ -87,7 +100,7 @@ public class HttpReceiver {
     public static void main(String[] args) {
         try {
             HttpReceiver receiver = new HttpReceiver(1234);
-            receiver.listenForCommands();
+            receiver.listenForMessages();
             receiver.close();
         } catch (IOException e) {
             e.printStackTrace();
--- src/httpchat/HttpSender.java
+++ src/httpchat/HttpSender.java
@@ -1,73 +1,88 @@
 package httpchat;
 
+
 import java.io.*;
 import java.net.Socket;
 
 public class HttpSender {
     private Socket socket;
-    private PrintWriter writer;
-    private BufferedReader reader;
+    private DataOutputStream outputStream;
     private static final int BUFFER_SIZE = 4096;
 
     public HttpSender(String address, int port) throws IOException {
         this.socket = new Socket(address, port);
-        this.writer = new PrintWriter(socket.getOutputStream(), true);
-        this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.outputStream = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+    }
+    
+    public void sendHttpRequest(String httpRequest) throws IOException {
+        outputStream.writeBytes(httpRequest);
+        outputStream.flush();
     }
 
-    public void sendCommand(int command, String content) throws IOException {
-        String request = "POST /command HTTP/1.1\r\n" +
-                "Host: localhost\r\n" +
-                "Content-Length: " + (content.length() + Integer.BYTES) + "\r\n" +  // Adding the size of the command
-                "Content-Type: text/plain\r\n" +
-                "\r\n" +
-                command +
-                content;
 
-        writer.print(request);
-        writer.flush();
+    public void openConnection() throws IOException {
+        String httpRequest = "GET /hello HTTP/1.1\r\n\r\n";
+        sendHttpRequest(httpRequest);
+    }
 
-        // Read the response from the receiver
-        String responseLine;
-        while ((responseLine = reader.readLine()) != null && !responseLine.isEmpty()) {
-            System.out.println(responseLine);
-        }
+    public void closeConnection() throws IOException {
+        String httpRequest = "GET /goodbye HTTP/1.1\r\n\r\n";
+        sendHttpRequest(httpRequest);
     }
 
     public void sendFile(String filePath) throws IOException {
         File file = new File(filePath);
         FileInputStream fis = new FileInputStream(file);
+
+        String httpRequest = "POST /file HTTP/1.1\r\n";
+        httpRequest += "Content-Length: " + (4 + file.getName().length() + 8 + file.length()) + "\r\n\r\n";
+        
+        sendHttpRequest(httpRequest);
+
+        outputStream.writeInt(2); // Command 2 indicates this is a file
+        outputStream.writeUTF(file.getName());
+        outputStream.writeLong(file.length());
+
         byte[] buffer = new byte[BUFFER_SIZE];
-        StringBuilder fileContent = new StringBuilder();
         int bytesRead;
 
         while ((bytesRead = fis.read(buffer)) != -1) {
-            fileContent.append(new String(buffer, 0, bytesRead));
+            outputStream.write(buffer, 0, bytesRead);
         }
 
         fis.close();
-
-        sendCommand(2, file.getName() + "\n" + file.length() + "\n" + fileContent);
+        outputStream.flush();
     }
 
+
+
+
+
     public void sendMessage(String message) throws IOException {
-        sendCommand(1, message);
+        String httpRequest = "POST /message HTTP/1.1\r\n";
+        httpRequest += "Content-Length: " + (4 + message.length() + 2) + "\r\n\r\n";
+        httpRequest += "1"; // Command 1 indicates this is a message
+        httpRequest += message;
+        
+        sendHttpRequest(httpRequest);
     }
 
     public void close() throws IOException {
-        reader.close();
-        writer.close();
+        outputStream.close();
         socket.close();
     }
 
     public static void main(String[] args) {
         try {
             HttpSender sender = new HttpSender("localhost", 1234);
+            sender.openConnection();
             sender.sendMessage("Hello!");
-            sender.sendFile("C:\\JavaProjects\\chatclient\\src\\chatclient\\test.zip");
+            sender.sendFile("C:\\JavaProjects\\chatclient\\src\\chatclient\\test.zip"); 
+            sender.closeConnection();
             sender.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
+
